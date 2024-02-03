040e406afe9191d171898d89c44577c793019d9e
Overlay404 <denis.alekseev.200411@mail.ru>
2024-02-03 17:16:47 +0300

    First commit

---
+++ TestBlazorApplication/Model/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace TestBlazorApplication.Model
+{
+    public class Country
+    {
+        public string? CountryCode { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ TestBlazorApplication/Model/Holiday.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace TestBlazorApplication.Model
+{
+    public class Holiday
+    {
+        public string? Name { get; set; }
+        public string? CountryCode { get; set; }
+        public string? LocalName { get; set;}
+        public DateTime Date { get; set;}
+        public bool Fixed {  get; set;}
+        public bool Global { get; set;}
+        public string? LaunchYear { get; set;}
+        public List<string>? Types { get; set;}
+    }
+}
